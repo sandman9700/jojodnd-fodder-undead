e7798c88210865f7d87413fe5d34ff16c27b6983
sandman9700 <150147390+sandman9700@users.noreply.github.com>
2024-08-13 11:30:53 -0400

    image

--- fodder.js
+++ fodder.js
@@ -1,85 +1,93 @@
 var Damage = "";
-$('#DMG').change(function() {
-  Damage = $('#DMG').val();
-  console.log("DMG:", Damage)
+$('#DMG').change(function () {
+    Damage = $('#DMG').val();
+    console.log("Damage:", Damage)
+});
+
+var Name = "";
+$('#NAME').change(function () {
+    Name = $('#NAME').val();
+    console.log("Name:", Name)
 });
 
 var Misc = "";
-$('#MISC').change(function() {
-  Misc = $('#MISC').val();
-  console,log("MISC:", Misc)
+$('#MISC').change(function () {
+    Misc = $('#MISC').val();
+    console.log("MISC:", Misc)
 });
 
 var Hit = 12;
-$('.HitSelect').change(function() {
-  Hit = parseInt($('.HitSelect').val());
-  updateHP();
+$('.HitSelect').change(function () {
+    Hit = parseInt($('.HitSelect').val());
+    HitDie = "d" + Hit
+    updateHP();
 });
 
 var PB = 2;
-$('.PBSelect').change(function() {
-  PB = parseInt($('.PBSelect').val());
-  updateHP();
-  updateAC();
+$('.PBSelect').change(function () {
+    PB = parseInt($('.PBSelect').val());
+    PRB = "+" + PB
+    updateHP();
+    updateAC();
 });
 
 var Str = 0;
-$('#STR').change(function() {
-  Str = parseInt($('#STR').val());
-  console.log("Strength:", Str);
+$('#STR').change(function () {
+    Str = parseInt($('#STR').val());
+    console.log("Strength:", Str);
 });
 
 var Dex = 0;
-$('#DEX').change(function() {
-  Dex = parseInt($('#DEX').val());
-  updateAC();
-  console.log("Dexterity:", Dex);
+$('#DEX').change(function () {
+    Dex = parseInt($('#DEX').val());
+    updateAC();
+    console.log("Dexterity:", Dex);
 });
 
 var Con = 0;
-$('#CON').change(function() {
-  Con = parseInt($('#CON').val());
-  updateHP();
-  updateAC();
-  console.log("Constitution:", Con);
+$('#CON').change(function () {
+    Con = parseInt($('#CON').val());
+    updateHP();
+    updateAC();
+    console.log("Constitution:", Con);
 });
 
 var Int = 0;
-$('#INT').change(function() {
-  Int = parseInt($('#INT').val());
-  console.log("Intelligence:", Int);
+$('#INT').change(function () {
+    Int = parseInt($('#INT').val());
+    console.log("Intelligence:", Int);
 });
 
 var Wis = 0;
-$('#WIS').change(function() {
-  Wis = parseInt($('#WIS').val());
-  updateAC();
-  console.log("Wisdom:", Wis);
+$('#WIS').change(function () {
+    Wis = parseInt($('#WIS').val());
+    updateAC();
+    console.log("Wisdom:", Wis);
 });
 
 var Cha = 0;
-$('#CHA').change(function() {
-  Cha = parseInt($('#CHA').val());
-  console.log("Charisma:", Cha);
+$('#CHA').change(function () {
+    Cha = parseInt($('#CHA').val());
+    console.log("Charisma:", Cha);
 });
 
-function updateHP(){
+function updateHP() {
     var HPBox = $('#HP'),
-    HP = "",
-    Roll = Math.floor(Math.random() * Hit) + 1;
+        HP = "",
+        Roll = Math.floor(Math.random() * Hit) + 1;
 
     HP = Hit + (Roll * PB) + (Con * PB)
 
-HPBox.html(HP);
-console.log("PB:", PB, "Hit:", Hit, "Roll:", Roll, "Con:", Con, "HP:", HP);
+    HPBox.html(HP);
+    console.log("PB:", PB, "Hit:", Hit, "Roll:", Roll, "Con:", Con, "HP:", HP);
 }
 updateHP();
 
-function updateAC () {
+function updateAC() {
     var ACBox = $('#AC'),
-    HACBox = $('#HAC'),
-    highestStat = Math.max(Dex, Con, Wis), // Get the highest of Dex, Con, or Wis
-    AC = 10 + PB + highestStat;
+        HACBox = $('#HAC'),
+        highestStat = Math.max(Dex, Con, Wis), // Get the highest of Dex, Con, or Wis
+        AC = 10 + PB + highestStat;
     HAC = AC + PB;
 
     ACBox.html(AC);
@@ -88,31 +96,37 @@ function updateAC () {
 }
 updateAC();
 
-$(document).ready(function() {
-    $('#attributes').change(function() {
+$(document).ready(function () {
+    $('#attributes').change(function () {
         var selectedValue = $(this).val();
-        var selectedText = $(this).find('option:selected').text();
+        var spec = $(this).find('option:selected').text();
         console.log("Selected Value: " + selectedValue);
-        console.log("Selected Text: " + selectedText);
+        console.log("Selected Text: " + spec);
+    });
+});
+$(document).ready(function () {
+    $('#Export').click(function () {
+        var final = "<table id='tableCapture'><thead><tr><th colspan='5'>Name: " + Name + "</th><th>Hit Die: " + HitDie + "</th></tr></thead><tbody><tr><td colspan='2'>Specialization: " + spec + "</td><td>HP: " + HP + "</td><td>AC: " + AC + "</td><td>HAC: " + HAC + "</td><td>PB: " + PRB + "</td></tr><tr><td>STR</td><td>DEX</td><td>CON</td><td>INT</td><td>WIS</td><td>CHA</td></tr><tr><td>" + Str + "</td><td>" + Dex + "</td><td>" + Con + "</td><td>" + Int + "</td><td>" + Wis + "</td><td>" + Cha + "</td></tr><tr><td colspan='3'>Damage: " + Damage + "</td><td colspan='3'>Misc: " + Misc + "</td></tr></tbody></table>"
+        $("#display").html(final)
     });
 });
 
-document.getElementById('download-btn').addEventListener('click', function() {
-    html2canvas(document.getElementById('input-table')).then(function(canvas) {
+document.getElementById('download-btn').addEventListener('click', function () {
+    html2canvas(document.getElementById('tableCapture')).then(function (canvas) {
         // Convert the canvas to a data URL
         var imgData = canvas.toDataURL('image/png');
-        
+
         // Create a link element
         var link = document.createElement('a');
         link.href = imgData;
-        link.download = 'table-image.png';
-        
+        link.download = Name + '.png';
+
         // Append the link to the body
         document.body.appendChild(link);
-        
+
         // Simulate a click on the link to trigger the download
         link.click();
-        
+
         // Remove the link from the body
         document.body.removeChild(link);
     });
